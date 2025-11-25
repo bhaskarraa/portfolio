3417ecc5105a0ca7f8183894821bc39f8641ffd3
bhaskarraa <sabdabhaskara86@gmail.com>
2025-11-25 22:01:56 +0700

    fix: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://bhskarraa.github.io',
+	base: '/',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
